9e55bab5dc14aa2ab335665d6b5a3a6f29975a30
GV Prashanth <36256971+gv-prashanth@users.noreply.github.com>
2019-11-02 19:49:03 +0530

    test code

--- motionalarm-pir-speaker/motionalarm-pir-speaker.ino
+++ motionalarm-pir-speaker/motionalarm-pir-speaker.ino
@@ -1,16 +1,39 @@
-const int pirInterruptPin = 2;//pin 2 only should be used
+/*
+ * PIR sensor tester
+ */
 const int speakerPin = 4;
 const int talkFrequency = 2000;//frequency in Hz
-
+int ledPin = 13;                // choose the pin for the LED
+int inputPin = 2;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+ 
 void setup() {
-  Serial.begin (9600);
-  pinMode(pirInterruptPin, INPUT);     // declare sensor as input
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+ 
+  Serial.begin(9600);
 }
-
-void loop() {
-  int val = digitalRead(pirInterruptPin);  // read input value
+ 
+void loop(){
+  val = digitalRead(inputPin);  // read input value
   if (val == HIGH) {            // check if the input is HIGH
-    tone(speakerPin, talkFrequency, 5000);
-    delay(5000);
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+    if (pirState == LOW) {
+      // we have just turned on
+      Serial.println("Motion detected!");
+      tone(speakerPin, talkFrequency);
+      // We only want to print on the output change, not state
+      pirState = HIGH;
+    }
+  } else {
+    digitalWrite(ledPin, LOW); // turn LED OFF
+    if (pirState == HIGH){
+      // we have just turned of
+      Serial.println("Motion ended!");
+      noTone(speakerPin);
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
   }
 }
