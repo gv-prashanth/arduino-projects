5e31461052024eac9e52416565df62685d1ca062
GV Prashanth <36256971+gv-prashanth@users.noreply.github.com>
2019-11-04 19:31:33 +0530

    adding power pin for testing

--- motionalarm-pir-speaker/motionalarm-pir-speaker.ino
+++ motionalarm-pir-speaker/motionalarm-pir-speaker.ino
@@ -4,14 +4,16 @@
 const int speakerPin = 4;
 const int talkFrequency = 2000;//frequency in Hz
 int ledPin = 13;                // choose the pin for the LED
+int powerPin = 3;               // choose the power pin (for PIR sensor)
 int inputPin = 2;               // choose the input pin (for PIR sensor)
 int pirState = LOW;             // we start, assuming no motion detected
 int val = 0;                    // variable for reading the pin status
  
 void setup() {
   pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(powerPin, OUTPUT);      // declare LED as output
   pinMode(inputPin, INPUT);     // declare sensor as input
- 
+  digitalWrite(powerPin, HIGH);
   Serial.begin(9600);
 }
  
