a1b6a150834ccc1b5b546149bff104340d6e99b1
Gumma <prvenkat@lowes.com>
2019-12-16 12:55:57 +0530

    using different mag library

--- roamingrobot-dualwheelbase-fixedhead-ultrasonic-voltage-pir-mag/roamingrobot-dualwheelbase-fixedhead-ultrasonic-voltage-pir-mag.ino
+++ roamingrobot-dualwheelbase-fixedhead-ultrasonic-voltage-pir-mag/roamingrobot-dualwheelbase-fixedhead-ultrasonic-voltage-pir-mag.ino
@@ -4,8 +4,10 @@
 #include <MorseCode.h>
 #include <DeepSleep.h>
 #include <Wire.h>
-#include <HMC5883L.h>
-//You can download the driver from https://github.com/Seeed-Studio/Grove_3Axis_Digital_Compass_HMC5883L
+#include <Adafruit_Sensor.h>
+//You can download the driver from https://github.com/adafruit/Adafruit_Sensor
+#include <Adafruit_HMC5883_U.h>
+//You can download the driver from https://github.com/adafruit/Adafruit_HMC5883_Unified
 #include <PID_v1.h>
 //You can download the driver from https://github.com/br3ttb/Arduino-PID-Library/
 
@@ -35,6 +37,8 @@ const float smallR = 10000.0;//Ohms. It is Voltage sensor smaller Resistance val
 const float bigR = 10000.0;//Ohms. It is Voltage sensor bigger Resistance value. Usually the one connected to sense.
 const float basePower = 0.5;//0.0 to 1.0
 double Kp = 2, Ki = 5, Kd = 1; //Specify the links and initial tuning parameters
+//TODO: need to get rid of below
+const float permittedAngularVariance = 5.0;
 
 //Dont touch below stuff
 unsigned long lastDirectionChangedTime = 0;
@@ -48,7 +52,7 @@ UltrasonicSensor ultrasonicSensor(ultraTriggerPin, ultraEchoPin);
 DualWheelBase base(leftWheelForwardPin, leftWheelBackwardPin, rightWheelForwardPin, rightWheelBackwardPin);
 MorseCode morseCode(speakerPin, talkFrequency, morseUnit);
 DeepSleep deepSleep;
-HMC5883L compass;
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 PID LmyPID(&LInput, &LOutput, &LSetpoint, Kp, Ki, Kd, DIRECT);
 PID RmyPID(&RInput, &ROutput, &RSetpoint, Kp, Ki, Kd, DIRECT);
 
@@ -58,7 +62,6 @@ void setup() {
   pinMode(smartPowerPin, OUTPUT);
 
   Wire.begin();
-  compass = HMC5883L(); //new instance of HMC5883L library
   setupHMC5883L(); //setup the HMC5883L
 
   LmyPID.SetMode(AUTOMATIC);
@@ -80,7 +83,7 @@ void setup() {
     doBIOSManoeuvre();
   }
 
-  destinationHeading = getAvgHeading();
+  destinationHeading = getHeading();
 }
 
 void loop() {
@@ -285,63 +288,100 @@ boolean decideOnRight() {
   }
 }
 
-float getAvgHeading() {
-  int NumOfAttempts = 10;
-  float toReturnAvg = 0.0;
-  for (int i = 1; i <= NumOfAttempts; i++) {
-    float thisHeadingToAvg = getHeading();
-    toReturnAvg = toReturnAvg + thisHeadingToAvg;
-    delay(10);
-  }
-  Serial.println("Mag Reading: " + String(toReturnAvg / NumOfAttempts));
-  return toReturnAvg / NumOfAttempts;
-}
-
 float getHeading() {
-  //Get the reading from the HMC5883L and calculate the heading
-  MagnetometerScaled scaled = compass.readScaledAxis(); //scaled values from compass.
-  float heading = atan2(scaled.YAxis, scaled.XAxis);
+  /* Get a new sensor event */
+  sensors_event_t event;
+  mag.getEvent(&event);
+
+  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
+  Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  "); Serial.println("uT");
+
+  // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
+  // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  float heading = atan2(event.magnetic.y, event.magnetic.x);
+
+  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
+  // Find yours here: http://www.magnetic-declination.com/
+  // Mine is: -13* 2' W, which is ~13 Degrees, or (which we need) 0.22 radians
+  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
+  float declinationAngle = 0.22;
+  heading += declinationAngle;
 
   // Correct for when signs are reversed.
-  if (heading < 0) heading += 2 * PI;
-  if (heading > 2 * PI) heading -= 2 * PI;
-  //Serial.println("Mag Reading: " + String(heading * RAD_TO_DEG));
-  return heading * RAD_TO_DEG; //radians to degrees
+  if (heading < 0)
+    heading += 2 * PI;
+
+  // Check for wrap due to addition of declination.
+  if (heading > 2 * PI)
+    heading -= 2 * PI;
+
+  // Convert radians to degrees for readability.
+  float headingDegrees = heading * 180 / M_PI;
+
+  Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
+  return headingDegrees;
+
 }
 
 void setupHMC5883L() {
-  //Setup the HMC5883L, and check for errors
-  int error;
-  error = compass.setScale(1.3); //Set the scale of the compass.
-  if (error != 0) Serial.println(compass.getErrorText(error)); //check if there is an error, and print if so
+  Serial.println("HMC5883 Magnetometer Test"); Serial.println("");
 
-  error = compass.setMeasurementMode(MEASUREMENT_CONTINUOUS); // Set the measurement mode to Continuous
-  if (error != 0) Serial.println(compass.getErrorText(error)); //check if there is an error, and print if so
+  /* Initialise the sensor */
+  if (!mag.begin())
+  {
+    /* There was a problem detecting the HMC5883 ... check your connections */
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    while (1);
+  }
+
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+}
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  mag.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
 }
 
 void goTowardsDestination() {
   float rightAngleDiff = rightAngularDifference();
   float leftAngleDiff = leftAngularDifference();
-  if (rightAngleDiff < leftAngleDiff) {
+  if (rightAngleDiff > permittedAngularVariance && rightAngleDiff < leftAngleDiff) {
     //easier to fix by steering left
     LSetpoint = 0;
     LInput = getAngularDiffIn255Scale(rightAngleDiff);
     LmyPID.Compute();
     Serial.println("Left Input: " + String(getAngularDiffIn255Scale(rightAngleDiff)) + " & Output: " + LOutput);
     base.steerLeft(LOutput);
-  } else {
+  } else if (leftAngleDiff > permittedAngularVariance && leftAngleDiff < rightAngleDiff) {
     //easier to fix by steering right
     RSetpoint = 0;
     RInput = getAngularDiffIn255Scale(leftAngleDiff);
     RmyPID.Compute();
     Serial.println("Right Input: " + String(getAngularDiffIn255Scale(leftAngleDiff)) + " & Output: " + ROutput);
     base.steerRight(ROutput);
+  } else {
+    Serial.println("Not much deviation. Will go straight");
+    base.goForward();
   }
 }
 
 float rightAngularDifference() {
   float toReturn = -1.0;
-  float currentHeading = getAvgHeading();
+  float currentHeading = getHeading();
   if (destinationHeading > 180 && destinationHeading < 360 && currentHeading > 0 && currentHeading < 180) {
     toReturn = (360.0 - destinationHeading) + currentHeading;
   } else if (currentHeading > destinationHeading) {
@@ -352,7 +392,7 @@ float rightAngularDifference() {
 
 float leftAngularDifference() {
   float toReturn = -1.0;
-  float currentHeading = getAvgHeading();
+  float currentHeading = getHeading();
   if (destinationHeading > 0 && destinationHeading < 180 && currentHeading > 180 && currentHeading < 360) {
     toReturn = (360.0 - currentHeading) + destinationHeading;
   } else if (currentHeading < destinationHeading) {
