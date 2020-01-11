5be8b965013bd91e74991b282b33c67d58aa9dc8
GV Prashanth <36256971+gv-prashanth@users.noreply.github.com>
2020-01-11 17:05:23 +0530

    using MPU

--- libraries/PIDTuner/examples/PIDTuner/PIDTuner.ino
+++ libraries/PIDTuner/examples/PIDTuner/PIDTuner.ino
@@ -1,29 +1,44 @@
 #include <DualWheelBase.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-//You can download the driver from https://github.com/adafruit/Adafruit_Sensor
-#include <Adafruit_HMC5883_U.h>
-//You can download the driver from https://github.com/adafruit/Adafruit_HMC5883_Unified
-#include <PID_v1.h>
-//You can download the driver from https://github.com/br3ttb/Arduino-PID-Library/
+#include <PID_v1.h> //You can download the driver from https://github.com/br3ttb/Arduino-PID-Library/
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h" //You can download the driver from https://github.com/electroniccats/mpu6050
+#include "Wire.h"
 
 //Pin Configuration
-const int leftWheelForwardPin = 5;//5
+const int leftWheelForwardPin = 6;//5
 const int leftWheelBackwardPin = 9;//9
-const int rightWheelForwardPin = 11;//10
-const int rightWheelBackwardPin = 10;//11
-const int smartPowerPin = 6; //can be used to sleep and wake peripherals
+const int rightWheelForwardPin = 10;//10
+const int rightWheelBackwardPin = 11;//11
+const int smartPowerPin = 12; //can be used to sleep and wake peripherals
 
 //functional Configuration
-const float basePower = 0.5;//0.0 to 1.0
-double Kp = 1.4, Ki = 0, Kd = 0; //Specify the links and initial tuning parameters
+const float basePower = 0.8;//0.0 to 1.0
+double Kp = 2, Ki = 0.2, Kd = 0.6; //Specify the links and initial tuning parameters
 
 //Dont touch below stuff
 float destinationHeading;
 double Setpoint, Input, Output;//Define Variables we'll be connecting to
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
 DualWheelBase base(leftWheelForwardPin, leftWheelBackwardPin, rightWheelForwardPin, rightWheelBackwardPin);
-Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+MPU6050 mpu;
 
 void setup() {
   Serial.begin (9600);
@@ -31,90 +46,147 @@ void setup() {
   pinMode(smartPowerPin, OUTPUT);
   digitalWrite(smartPowerPin, HIGH);
   
-  Wire.begin();
-  setupHMC5883L(); //setup the HMC5883L
-
   myPID.SetMode(AUTOMATIC);
   myPID.SetOutputLimits(-255, 255);
 
   //TODO: Setting base power to a fixed value. Need to make dynamic
   base.setPowerMultiplier(basePower);
 
-  delay(3000);
+  setupMPU();
+  stabilizeMPU();
+  
   destinationHeading = getHeading();
   Serial.println("BIOS complete. Setting heading to " + String(destinationHeading));
 }
 
 void loop() {
+  populateYPR();
   goTowardsDestination();
 }
 
 float getHeading() {
-  /* Get a new sensor event */
-  sensors_event_t event;
-  mag.getEvent(&event);
-
-  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
-  //Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
-  //Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
-  //Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  "); Serial.println("uT");
-
-  // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
-  // Calculate heading when the magnetometer is level, then correct for signs of axis.
-  float heading = atan2(event.magnetic.y, event.magnetic.x);
-
-  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
-  // Find yours here: http://www.magnetic-declination.com/
-  // Mine is: -13* 2' W, which is ~13 Degrees, or (which we need) 0.22 radians
-  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
-  float declinationAngle = 0.22;
-  heading += declinationAngle;
-
-  // Correct for when signs are reversed.
-  if (heading < 0)
-    heading += 2 * PI;
-
-  // Check for wrap due to addition of declination.
-  if (heading > 2 * PI)
-    heading -= 2 * PI;
-
-  // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180 / M_PI;
-
-  Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
-  return headingDegrees;
-
-}
-
-void setupHMC5883L() {
-  Serial.println("HMC5883 Magnetometer Test"); Serial.println("");
-
-  /* Initialise the sensor */
-  if (!mag.begin())
-  {
-    /* There was a problem detecting the HMC5883 ... check your connections */
-    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
-    while (1);
+  if (ypr[0] > 0) {
+    //Serial.println(ypr[0] * 180 / M_PI);
+    return ypr[0] * 180 / M_PI;
   }
-
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
+  else {
+    //Serial.println((180 - abs(ypr[0] * 180 / M_PI)) + 180);
+    return (180 - abs(ypr[0] * 180 / M_PI)) + 180;
+  }
 }
 
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  mag.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
+//setup the mpu
+void setupMPU() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  Wire.begin();
+  TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+
+  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
+  // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
+  // the baud timing being too misaligned with processor ticks. You must use
+  // 38400 or slower in these cases, or use some kind of external separate
+  // crystal solution for the UART timer.
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+    while (true);
+  }
+}
+
+void stabilizeMPU() {
+  Serial.println("Stabilizing MPU...");
+  unsigned long currentTime = millis();
+  while (millis() - currentTime < 30000) {
+    populateYPR();
+  }
+  Serial.println("MPU stabilization complete");
+}
+
+//dmp interrupt
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+//populates the ypr based on interrupt condition
+void populateYPR() {
+  // wait for MPU interrupt or extra packet(s) available
+  if (mpuInterrupt || fifoCount > packetSize) {
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+      // reset so we can continue cleanly
+      mpu.resetFIFO();
+      Serial.println(F("FIFO overflow!"));
+
+      // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+      // wait for correct available data length, should be a VERY short wait
+      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+      // read a packet from FIFO
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+//      Serial.print("ypr\t");
+//      Serial.print(ypr[0] * 180 / M_PI);
+//      Serial.print("\t");
+//      Serial.print(ypr[1] * 180 / M_PI);
+//      Serial.print("\t");
+//      Serial.println(ypr[2] * 180 / M_PI);
+    }
+  }
 }
 
 void goTowardsDestination() {
@@ -122,11 +194,17 @@ void goTowardsDestination() {
   Setpoint = 0;
   Input = angleDiff;
   myPID.Compute();
-  float powerDiff = Output;
-  Serial.println("PID Input: " + String(angleDiff) + " & Output: " + powerDiff + " will steer to fix the problem");
   //If powerDiff is positive i need to steer left
   //If powerDiff is negative i need to steer right
-  base.goForward(powerDiff);
+  float powerDiff = Output;
+  //Serial.println("PID Input: " + String(angleDiff) + " & Output: " + powerDiff + " will steer to fix the problem");
+  if (powerDiff < -254) {
+    base.rotateRight();
+  } else if (powerDiff > 254) {
+    base.rotateLeft();
+  } else {
+    base.goForward(powerDiff);
+  }
 }
 
 float calculateAngularDifferenceVector() {
@@ -135,7 +213,6 @@ float calculateAngularDifferenceVector() {
   if (currentHeading >= destinationHeading) {
     float left = currentHeading - destinationHeading;
     float right = (360.0 - currentHeading) + destinationHeading;
-    Serial.println("Going towards destination. Bios Set heading is " + String(destinationHeading) + ". current heading is " + String(currentHeading) + ". Diff is left " + String(left) + " right " + String(right));
     if (left < right)
       return -1 * left;
     else
@@ -143,7 +220,6 @@ float calculateAngularDifferenceVector() {
   } else {
     float left = (360.0 - destinationHeading) + currentHeading;
     float right = destinationHeading - currentHeading;
-    Serial.println("Going towards destination. Bios Set heading is " + String(destinationHeading) + ". current heading is " + String(currentHeading) + ". Diff is left " + String(left) + " right " + String(right));
     if (left < right)
       return -1 * left;
     else
