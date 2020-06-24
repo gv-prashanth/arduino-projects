4603dead9e85345374355962c11baf6b81bffa9f
Gumma <prvenkat@lowes.com>
2020-06-24 21:11:35 +0530

    integrated with roamigrobot code

--- roamingrobot-selfbalancebase-servohead-ultrasonic-voltage-mpu/roamingrobot-selfbalancebase-servohead-ultrasonic-voltage-mpu.ino
+++ roamingrobot-selfbalancebase-servohead-ultrasonic-voltage-mpu/roamingrobot-selfbalancebase-servohead-ultrasonic-voltage-mpu.ino
@@ -1,10 +1,21 @@
 #include <Wire.h>                                            //Include the Wire.h library so we can communicate with the gyro
+#include <VoltageSensor.h>
 
 //Pin Configuration
+const int batteryVoltageSensePin = -1;//A2 incase you want to detect from dedicated pin. -1 incase you want to detect from vcc.
 const int TRIGGER_PIN = 7; // Arduino pin tied to trigger pin on ping sensor.
 const int ECHO_PIN = 6; // Arduino pin tied to echo pin on ping sensor.
 
 //functional Configuration
+const int avoidableObstacleRange = 60;//cm
+const int emergencyObstacleRange = avoidableObstacleRange / 3; //cm
+const int timeToStickRightLeftDecission = 5000;//milli seconds
+const unsigned long robotJamCheckTime = 120000; //milli seconds
+const int baseMovementTime = 1500;//milli seconds
+const float sleepVoltage = 3.3;//volts
+const float wakeVoltage = 3.7;//volts. Must be greater than sleepVoltage.
+const float smallR = 10000.0;//Ohms. It is Voltage sensor smaller Resistance value. Usually the one connected to ground.
+const float bigR = 10000.0;//Ohms. It is Voltage sensor bigger Resistance value. Usually the one connected to sense.
 float pid_p_gain = 15;                                       //Gain setting for the P-controller (15)
 float pid_i_gain = 1.5;                                      //Gain setting for the I-controller (1.5)
 float pid_d_gain = 30;                                       //Gain setting for the D-controller (30)
@@ -12,8 +23,6 @@ float turning_speed = 5;                                    //Turning speed (20)
 float max_target_speed = 5;                                //Max target speed (100)
 int acc_calibration_value = 675;                            //Enter the accelerometer calibration value
 int gyro_address = 0x68;                                     //MPU-6050 I2C address (0x68 or 0x69)
-
-const int timeToStickRightLeftDecission = 5000;//milli seconds
 
 //Dont touch below stuff
 byte start, received_byte, low_bat;
@@ -28,23 +37,249 @@ float angle_gyro, angle_acc, angle, self_balance_pid_setpoint;
 float pid_error_temp, pid_i_mem, pid_setpoint, gyro_input, pid_output, pid_last_d_error;
 float pid_output_left, pid_output_right;
 
-byte commandToRun;
-unsigned long commandEndTime, botStartTime, lastSonarSentTime, lastEchoReceivedTime;
+unsigned long lastComandedDirectionChangeTime = 0;
+unsigned long overrideForwardUntill = 0;
 unsigned long lastRightLeftDecidedTime = 0;
 boolean isRightDecidedCached = false;
+float destinationHeading = 0.0;
+float centerReading = 0.0;
+
+byte commandToRun;
+unsigned long commandEndTime, botStartTime, lastSonarSentTime, lastEchoReceivedTime;
+
+VoltageSensor batteryVoltageSensor(batteryVoltageSensePin, smallR, bigR);
 
 void setup() {
+
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   Serial.begin(9600);
   setupBase();
   botStartTime = millis();
   lastSonarSentTime = micros();
-  lastEchoReceivedTime = micros()+10000;
+  lastEchoReceivedTime = micros() + 10000;
+
+  //Wake the robot
+  markForWakeup();
+
+  //Print the voltage of battery
+  Serial.println("Battery Voltage: " + String(batteryVoltageSensor.senseVoltage()));
+
+  //check if battery is low and skip bios dance
+  if (!isBatteryDead()) {
+    doBIOSManoeuvre();
+  }
 }
 
 void loop() {
-  long obstacleDist = (lastEchoReceivedTime - lastSonarSentTime) / 58.2;
+  populateUltrasonicReading();
+
+  //check if battery is low and go to sleep
+  if (isBatteryDead()) {
+    markForSleep();
+    return;
+  }
+
+  //TODO: Need to better check if there is a robot jam
+  else if (isJamDetected()) {
+    morseCodePlay("Jam");
+    markForForwardOverride(baseMovementTime);
+    setJamDestination();
+  }
+
+  //incase of emergency
+  else if (isObstacleWithinEmergencyDistance()) {
+    morseCodePlay("Emergency");
+    markForForwardOverride(baseMovementTime);
+    setEmergencyDestination();
+  }
+
+  //Only do this when you are moving forward
+  else if (isObstacleWithinAvoidableDistance()) {
+    //below is true means im going forward
+    if (!isForwardOverridden())
+      setAvoidableObstacleDestination();
+  }
+
+  rotateOrSteerAndGoTowardsDestination();
+
+  loopBase();
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //Loop time timer
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //The angle calculations are tuned for a loop time of 4 milliseconds. To make sure every loop is exactly 4 milliseconds a wait loop
+  //is created by setting the loop_timer variable to +4000 microseconds every loop.
+  while (loop_timer > micros());
+  loop_timer += 4000;
+
+}
+
+void markForSleep() {
+  morseCodePlay("SOS");
+}
+
+//TODO: Need to get rid of marks logic
+void markForWakeup() {
+  morseCodePlay("A");
+  lastComandedDirectionChangeTime = millis();
+  overrideForwardUntill = millis();
+}
+
+void markForForwardOverride(unsigned long overrideTime) {
+  overrideForwardUntill = millis() + overrideTime;
+}
+
+boolean isForwardOverridden() {
+  return overrideForwardUntill > millis();
+}
+
+boolean isBatteryCharged() {
+  return batteryVoltageSensor.senseVoltage() > wakeVoltage;
+}
+
+boolean isJamDetected() {
+  return abs(millis() - lastComandedDirectionChangeTime) > robotJamCheckTime;
+}
+
+boolean isBatteryDead() {
+  return batteryVoltageSensor.senseVoltage() < sleepVoltage;
+}
+
+boolean isObstacleWithinAvoidableDistance() {
+  return (centerReading > emergencyObstacleRange && centerReading <= avoidableObstacleRange);
+}
+
+boolean isObstacleWithinEmergencyDistance() {
+  return (centerReading > 0 && centerReading <= emergencyObstacleRange);
+}
+
+boolean isRightDecided() {
+  if (millis() - lastRightLeftDecidedTime > timeToStickRightLeftDecission) {
+    isRightDecidedCached = (random(0, 2) < 1);
+    lastRightLeftDecidedTime = millis();
+  }
+  return isRightDecidedCached;
+}
+
+void setEmergencyDestination() {
+  if (isRightDecided())
+    setRightDestinationByAngle(random(9, 18) * 10);
+  else
+    setLeftDestinationByAngle(random(9, 18) * 10);
+}
+
+void setJamDestination() {
+  if (isRightDecided())
+    setRightDestinationByAngle(random(1, 36) * 10);
+  else
+    setLeftDestinationByAngle(random(1, 36) * 10);
+}
+
+void setAvoidableObstacleDestination() {
+  if (isRightDecided())
+    setRightDestinationByAngle(random(1, 9) * 10);
+  else
+    setLeftDestinationByAngle(random(1, 9) * 10);
+}
+
+void doBIOSManoeuvre() {
+
+  while (millis() - botStartTime < 10000) {
+    baseStop();
+    loopBase();
+    while (loop_timer > micros());
+    loop_timer += 4000;
+  }
+
+  /*
+    //left
+    morseCodePlay("L");
+    baseRotateLeft();
+    delay(baseMovementTime);
+    baseStop();
+
+    //right
+    morseCodePlay("R");
+    baseRotateRight();
+    delay(baseMovementTime);
+    baseStop();
+
+    //forward
+    morseCodePlay("F");
+    baseGoForward();
+    delay(baseMovementTime);
+    baseStop();
+
+    //backward
+    morseCodePlay("B");
+    baseGoBackward();
+    delay(baseMovementTime);
+    baseStop();
+  */
+}
+
+void rotateOrSteerAndGoTowardsDestination() {
+  if (isForwardOverridden()) {
+    baseGoBackward();
+  } else if (calculateAngularDifferenceVector() > 0) {
+    baseRotateRight();
+    setRightDestinationByAngle(abs(calculateAngularDifferenceVector()) - 0.00001);
+  } else if (calculateAngularDifferenceVector() < 0) {
+    baseRotateLeft();
+    setLeftDestinationByAngle(abs(calculateAngularDifferenceVector()) + 0.00001);
+  } else {
+    baseGoForward();
+  }
+}
+
+float calculateAngularDifferenceVector() {
+  //The angular error is calculated by actual - required. Further the easisest / closest direction is choosen as part of returning the value.
+  return destinationHeading;
+}
+
+void setLeftDestinationByAngle(int angle) {
+  float currentHeading = getHeading();
+  destinationHeading = currentHeading - angle;
+  lastComandedDirectionChangeTime = millis();
+}
+
+void setRightDestinationByAngle(int angle) {
+  float currentHeading = getHeading();
+  destinationHeading = currentHeading + angle;
+  lastComandedDirectionChangeTime = millis();
+}
+
+void morseCodePlay(String toPlay) {
+  Serial.println(toPlay);
+}
+
+void baseRotateLeft() {
+  commandToRun = B00000001;
+}
+
+void baseRotateRight() {
+  commandToRun = B00000010;
+}
+
+void baseGoForward() {
+  commandToRun = B00000100;
+}
+
+void baseGoBackward() {
+  commandToRun = B00001000;
+}
+
+void baseStop() {
+  commandToRun = 0x00;
+}
+
+float getHeading() {
+  return 0;
+}
+
+void populateUltrasonicReading() {
+  centerReading = (lastEchoReceivedTime - lastSonarSentTime) / 58.2;
   //Serial.println(obstacleDist);
   if (micros() - lastSonarSentTime > 250000) {
     digitalWrite(TRIGGER_PIN, LOW);
@@ -54,54 +289,6 @@ void loop() {
     delayMicroseconds(17);
     digitalWrite(TRIGGER_PIN, LOW);
   }
-
-  if (millis() - botStartTime > 10000) {
-    if (obstacleDist < 80)
-      if(isRightDecided())
-        runCommandFor(B00000010, 8000);
-      else
-        runCommandFor(B00000001, 8000);
-    else
-      runCommandFor(B00000100, 1000);
-  }
-  loadCommand();
-  loopBase();
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //Loop time timer
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //The angle calculations are tuned for a loop time of 4 milliseconds. To make sure every loop is exactly 4 milliseconds a wait loop
-  //is created by setting the loop_timer variable to +4000 microseconds every loop.
-  while (loop_timer > micros());
-  loop_timer += 4000;
-}
-
-void runCommandFor(byte command, unsigned long timeToRun) {
-  /*
-    Left B00000001
-    Right B00000010
-    Forward B00000100
-    backward B00001000
-    DontKnow B00001100
-    DontKnow 0x00
-  */
-  commandEndTime = millis() + timeToRun;
-  commandToRun = command;
-}
-
-void loadCommand() {
-  if (commandEndTime > millis()) {
-    received_byte = commandToRun;
-  } else {
-    received_byte = 0x00;
-  }
-}
-
-boolean isRightDecided() {
-  if (millis() - lastRightLeftDecidedTime > timeToStickRightLeftDecission) {
-    isRightDecidedCached = (random(0, 2) < 1);
-    lastRightLeftDecidedTime = millis();
-  }
-  return isRightDecidedCached;
 }
 
 //Within interrupt. Should run fast.
