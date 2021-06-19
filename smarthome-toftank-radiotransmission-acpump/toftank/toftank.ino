c0d40c4b534514cd3a7c7012f990fdab50c2ade8
Gumma <3628612@lowes.com>
2021-06-19 20:50:19 +0530

    ultrasonic tof sensor

--- smarthome-toftank-radiotransmission-acpump/toftank/toftank.ino
+++ smarthome-toftank-radiotransmission-acpump/toftank/toftank.ino
@@ -6,7 +6,7 @@
    Using 433MHz RF Transmitter and Receiver modules for wireless link, along with RH_ASK to transmit the water level reading.
    Make sure you connect the RF Receiver to PIN 11 & RF Transmitter to PIN 12
    Arduino as logic controller to drive a sumpMotor Pump. Pump is  connected EM Relay mounted on Power supply unit.
-   Overhed tank is connected to a PWM based TOF distance sensor
+   Overhed tank is connected to a TOF distance sensor (Ultrasonic sensor)
    sumpMotorTriggerPin driver attached to pin 5  with 10k resistor to ground
 */
 
@@ -14,11 +14,16 @@
 #ifdef RH_HAVE_HARDWARE_SPI
 #include <SPI.h> // Not actually used but needed to compile
 #endif
+#include <DeepSleep.h>
+#include <NewPing.h>
+
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 RH_ASK driver;
-
-//Pin Configurations
-const int tofPWMPin = 5; // Over head tank water level measure TOF pin
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+DeepSleep deepSleep;
 
 void setup()
 {
@@ -31,17 +36,39 @@ void setup()
 #else
     ;
 #endif
-  pinMode(tofPWMPin, INPUT);
 }
 
 void loop()
 {
-  unsigned long duration = pulseIn(tofPWMPin, HIGH);
-  float distance = duration / 100.0;
+  //get average distance
+  float distance = getMeanDistance();
+
+  //send the message
   String distanceString = String(distance);
   const char *msg = distanceString.c_str();
   driver.send((uint8_t *)msg, strlen(msg));
   driver.waitPacketSent();
-  Serial.print(distance);Serial.println(" cm");
-  delay(200);
+  Serial.print(distance); Serial.println(" cm");
+
+  //sleep for aprox 1 min
+  for (int i = 0; i < 8; i++) {
+    deepSleep.sleepForEightSecondsUnlessInterrupted();
+  }
+}
+
+float getMeanDistance() {
+  float toAverageDistance = 0;
+  int validCounts = 0;
+  for (int i = 0; i < 5; i++) {
+    float thisDist = sonar.ping_cm();
+    if (thisDist != 0) {
+      toAverageDistance = toAverageDistance + thisDist;
+      validCounts++;
+    }
+    delay(50);
+  }
+  if (validCounts > 0)
+    return toAverageDistance / validCounts;
+  else
+    return 0;
 }
