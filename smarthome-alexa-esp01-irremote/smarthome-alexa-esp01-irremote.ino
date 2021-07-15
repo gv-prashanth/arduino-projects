a750c13e89c17b4432997a516265089cb2486eb8
Gumma <3628612@lowes.com>
2021-07-15 21:24:21 +0530

    Adding new smarthome alexa ir project

---
+++ smarthome-alexa-esp01-irremote/smarthome-alexa-esp01-irremote.ino
@@ -0,0 +1,148 @@
+/*
+ * Make sure you configure your wifi SSID & Password before loading
+ * Make sure you use Board version 2.5.0
+ * For two knobs to work use ZeroCross pin as 3. Although the PCB is designed with ZeroCross as zero pin.
+ */ 
+#ifdef ARDUINO_ARCH_ESP32
+#include <WiFi.h>
+#else
+#include <ESP8266WiFi.h>
+#endif
+//#define ESPALEXA_ASYNC            //uncomment for async operation (can fix empty body issue)
+//#define ESPALEXA_NO_SUBPAGE       //disable /espalexa status page
+//#define ESPALEXA_DEBUG            //activate debug serial logging
+//#define ESPALEXA_MAXDEVICES 15    //set maximum devices add-able to Espalexa
+#include <Espalexa.h>  // you can download the library from https://github.com/Aircoookie/Espalexa
+
+// Change this!!
+const char* ssid = "XXXXXX";
+const char* password = "YYYYYY";
+
+//Percentage initialization
+uint8_t percentA = 100;
+uint8_t percentB = 100;
+
+// prototypes
+bool connectWifi();
+
+//callback functions
+void knobCallbackA(EspalexaDevice* dev);
+void knobCallbackB(EspalexaDevice* dev);
+
+bool wifiConnected = false;
+
+Espalexa espalexa;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Initialise wifi connection
+  wifiConnected = connectWifi();
+  if(!wifiConnected){
+    while (1) {
+      Serial.println("Cannot connect to WiFi. Please check data and reset the ESP.");
+      delay(2500);
+    }
+  }
+  
+  // Define your first device here.
+  espalexa.addDevice("Fan", knobCallbackA, EspalexaDeviceType::dimmable, 127); //Dimmable device, optional 4th parameter is beginning state (here fully on)
+  espalexa.addDevice("Light", knobCallbackB, EspalexaDeviceType::dimmable, 127); //Dimmable device, optional 4th parameter is beginning state (here fully on)
+  espalexa.begin();
+}
+ 
+void loop()
+{
+ espalexa.loop();
+
+}
+
+//our callback functions
+void knobCallbackA(EspalexaDevice* d) {
+  if (d == nullptr) return;
+
+  uint8_t brightness = d->getValue();
+  percentA = d->getPercent();
+  uint8_t degrees = d->getDegrees(); //for heaters, HVAC, ...
+
+  if(d->getLastChangedProperty()== EspalexaDeviceProperty::off){
+    Serial.println("Looks like switch off command was invoked");
+  } else {
+    if(d->getLastChangedProperty()== EspalexaDeviceProperty::on){
+      Serial.println("Looks like switch on command was invoked");
+    }
+    if(d->getLastChangedProperty()== EspalexaDeviceProperty::bri){
+      Serial.println("Looks like percentage command was invoked");
+    }
+  }
+
+  Serial.print("Received value from alexa ");
+  Serial.print(brightness);
+  Serial.println(".");
+
+  Serial.print("Received percentage from alexa ");
+  Serial.print(percentA);
+  Serial.println("%");
+}
+
+//our callback functions
+void knobCallbackB(EspalexaDevice* d) {
+  if (d == nullptr) return;
+
+  uint8_t brightness = d->getValue();
+  percentB = d->getPercent();
+  uint8_t degrees = d->getDegrees(); //for heaters, HVAC, ...
+
+  if(d->getLastChangedProperty()== EspalexaDeviceProperty::off){
+    Serial.println("Looks like switch off command was invoked");
+  } else {
+    if(d->getLastChangedProperty()== EspalexaDeviceProperty::on){
+      Serial.println("Looks like switch on command was invoked");
+    }
+    if(d->getLastChangedProperty()== EspalexaDeviceProperty::bri){
+      Serial.println("Looks like percentage command was invoked");
+    }
+  }
+
+  Serial.print("Received value from alexa ");
+  Serial.print(brightness);
+  Serial.println(".");
+
+  Serial.print("Received percentage from alexa ");
+  Serial.print(percentB);
+  Serial.println("%");
+}
+
+// connect to wifi – returns true if successful or false if not
+bool connectWifi(){
+  bool state = true;
+  int i = 0;
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 20){
+      state = false; break;
+    }
+    i++;
+  }
+  Serial.println("");
+  if (state){
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    Serial.println("Connection failed.");
+  }
+  return state;
+}
