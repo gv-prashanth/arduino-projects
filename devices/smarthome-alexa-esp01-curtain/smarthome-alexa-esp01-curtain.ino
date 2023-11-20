ae42484e7a9879b71943f18fcf57b27ba30e9ff4
GV Prashanth <36256971+gv-prashanth@users.noreply.github.com>
2023-11-20 19:31:39 +0530

    Update smarthome-alexa-esp01-curtain.ino

--- devices/smarthome-alexa-esp01-curtain/smarthome-alexa-esp01-curtain.ino
+++ devices/smarthome-alexa-esp01-curtain/smarthome-alexa-esp01-curtain.ino
@@ -14,7 +14,8 @@
 const String DROID_ID = "ZZZ";
 #define SERIAL_BAUDRATE 115200
 #define CURTAIN "CURTAIN"
-const int step = 2, dir = 0, Enable = 4, irSensorPin = 15;
+String CURTAINKEY = "Curtain";
+const int step = 2, dir = 0, Enable = 1, irSensorPin = 3;
 
 // -----------------------------------------------------------------------------
 
@@ -23,16 +24,15 @@ const int step = 2, dir = 0, Enable = 4, irSensorPin = 15;
 fauxmoESP fauxmo;
 volatile boolean takeActionToSwitchOnCurtain, takeActionToSwitchOffCurtain;
 volatile int curtainValue = 255;
+volatile boolean prevLoop_isCurtainOpen, expected_isCurtainOpen;
 // -----------------------------------------------------------------------------
 
 void setup() {
 
   // Init serial port and clean garbage
-  Serial.begin(SERIAL_BAUDRATE);
-  Serial.println();
-  Serial.println();
+  //Serial.begin(SERIAL_BAUDRATE); Serial.println(); Serial.println();
 
-  // LEDs
+  // PINS
   pinMode(dir, OUTPUT);     // Stepper Motor Dir
   pinMode(step, OUTPUT);    // Stepper Motor Step
   pinMode(Enable, OUTPUT);  // Stepper Motor Enable
@@ -42,77 +42,19 @@ void setup() {
   // Wifi
   wifiSetup();
 
-  // By default, fauxmoESP creates it's own webserver on the defined port
-  // The TCP port must be 80 for gen3 devices (default is 1901)
-  // This has to be done before the call to enable()
-  fauxmo.createServer(true);  // not needed, this is the default value
-  fauxmo.setPort(80);         // This is required for gen3 devices
+  // fauxmo
+  fauxmoSetup();
 
-  // You have to call enable(true) once you have a WiFi connection
-  // You can enable or disable the library at any moment
-  // Disabling it will prevent the devices from being discovered and switched
-  fauxmo.enable(true);
-
-  // You can use different ways to invoke alexa to modify the devices state:
-  // "Alexa, turn yellow lamp on"
-  // "Alexa, turn on yellow lamp
-  // "Alexa, set yellow lamp to fifty" (50 means 50% of brightness, note, this example does not use this functionality)
-
-  // Add virtual devices
-  fauxmo.addDevice(CURTAIN);
-
-  fauxmo.onSetState([](unsigned char device_id, const char* device_name, bool state, unsigned char value) {
-    // Callback when a command from Alexa is received.
-    // You can use device_id or device_name to choose the element to perform an action onto (relay, LED,...)
-    // State is a boolean (ON/OFF) and value a number from 0 to 255 (if you say "set kitchen light to 50%" you will receive a 128 here).
-    // Just remember not to delay too much here, this is a callback, exit as soon as possible.
-    // If you have to do something more involved here set a flag and process it in your main loop.
-
-    Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
-
-    // Checking for device_id is simpler if you are certain about the order they are loaded and it does not change.
-    // Otherwise comparing the device_name is safer.
-
-    if (strcmp(device_name, CURTAIN) == 0) {
-      if (state == HIGH) {
-        takeActionToSwitchOnCurtain = true;
-      } else {
-        takeActionToSwitchOffCurtain = true;
-      }
-      curtainValue = (int)value;
-    }
-  });
+  // load the prev and expected status
+  prevLoop_isCurtainOpen = !isCurtainOpen();
+  expected_isCurtainOpen = isCurtainOpen();
 }
 
 void loop() {
-
-  // fauxmoESP uses an async TCP server but a sync UDP server
-  // Therefore, we have to manually poll for UDP packets
-  fauxmo.handle();
-
-  // This is a sample code to output free heap every 5 seconds
-  // This is a cheap way to detect memory leaks
-  static unsigned long last = millis();
-  if (millis() - last > 5000) {
-    last = millis();
-    Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
-  }
-
-  if (takeActionToSwitchOnCurtain) {
-    openCurtain();
-    sendSensorValueToAlexa("BackCurtain", "open");
-    takeActionToSwitchOnCurtain = false;
-  }
-
-  if (takeActionToSwitchOffCurtain) {
-    closeCurtain();
-    sendSensorValueToAlexa("BackCurtain", "closed");
-    takeActionToSwitchOffCurtain = false;
-  }
-
-  // If your device state is changed by any other means (MQTT, physical button,...)
-  // you can instruct the library to report the new state to Alexa on next request:
-  // fauxmo.setState(ID_YELLOW, true, 255);
+  fauxmoLoop();
+  checkAndTakeAction();
+  checkForErrorAndSendToAlexa();
+  checkForChangeAndSendToAlexa();
 }
 
 void wifiSetup() {
@@ -201,5 +143,97 @@ boolean isCurtainOpen() {
   } else {
     Serial.println("IR Proximity Sensor OFF");
     return true;
+  }
+}
+
+void fauxmoSetup() {
+  // By default, fauxmoESP creates it's own webserver on the defined port
+  // The TCP port must be 80 for gen3 devices (default is 1901)
+  // This has to be done before the call to enable()
+  fauxmo.createServer(true);  // not needed, this is the default value
+  fauxmo.setPort(80);         // This is required for gen3 devices
+
+  // You have to call enable(true) once you have a WiFi connection
+  // You can enable or disable the library at any moment
+  // Disabling it will prevent the devices from being discovered and switched
+  fauxmo.enable(true);
+
+  // You can use different ways to invoke alexa to modify the devices state:
+  // "Alexa, turn yellow lamp on"
+  // "Alexa, turn on yellow lamp
+  // "Alexa, set yellow lamp to fifty" (50 means 50% of brightness, note, this example does not use this functionality)
+
+  // Add virtual devices
+  fauxmo.addDevice(CURTAIN);
+
+  fauxmo.onSetState([](unsigned char device_id, const char* device_name, bool state, unsigned char value) {
+    // Callback when a command from Alexa is received.
+    // You can use device_id or device_name to choose the element to perform an action onto (relay, LED,...)
+    // State is a boolean (ON/OFF) and value a number from 0 to 255 (if you say "set kitchen light to 50%" you will receive a 128 here).
+    // Just remember not to delay too much here, this is a callback, exit as soon as possible.
+    // If you have to do something more involved here set a flag and process it in your main loop.
+
+    Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+
+    // Checking for device_id is simpler if you are certain about the order they are loaded and it does not change.
+    // Otherwise comparing the device_name is safer.
+
+    if (strcmp(device_name, CURTAIN) == 0) {
+      if (state == HIGH) {
+        takeActionToSwitchOnCurtain = true;
+      } else {
+        takeActionToSwitchOffCurtain = true;
+      }
+      curtainValue = (int)value;
+    }
+  });
+}
+
+void fauxmoLoop() {
+  // fauxmoESP uses an async TCP server but a sync UDP server
+  // Therefore, we have to manually poll for UDP packets
+  fauxmo.handle();
+
+  // This is a sample code to output free heap every 5 seconds
+  // This is a cheap way to detect memory leaks
+  static unsigned long last = millis();
+  if (millis() - last > 5000) {
+    last = millis();
+    Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
+  }
+}
+
+void checkForChangeAndSendToAlexa() {
+  // There was a change in the curtain status. Lets send it across to cloud and alexa
+  if (prevLoop_isCurtainOpen != isCurtainOpen()) {
+    // If your device state is changed by any other means (MQTT, physical button,...)
+    // you can instruct the library to report the new state to Alexa on next request:
+    //fauxmo.setState(CURTAIN, isCurtainOpen ? true : false, curtainValue);
+    sendSensorValueToAlexa(CURTAINKEY, isCurtainOpen() ? "open" : "closed");
+  }
+  prevLoop_isCurtainOpen = isCurtainOpen();
+}
+
+void checkForErrorAndSendToAlexa() {
+  if (expected_isCurtainOpen != isCurtainOpen()) {
+    // If your device state is changed by any other means (MQTT, physical button,...)
+    // you can instruct the library to report the new state to Alexa on next request:
+    //fauxmo.setState(CURTAIN, isCurtainOpen ? true : false, curtainValue);
+    sendSensorValueToAlexa(CURTAINKEY, "error");
+    expected_isCurtainOpen = isCurtainOpen();
+  }
+}
+
+void checkAndTakeAction() {
+  if (takeActionToSwitchOnCurtain) {
+    expected_isCurtainOpen = true;
+    openCurtain();
+    takeActionToSwitchOnCurtain = false;
+  }
+
+  if (takeActionToSwitchOffCurtain) {
+    expected_isCurtainOpen = false;
+    closeCurtain();
+    takeActionToSwitchOffCurtain = false;
   }
 }
